6ced383191e18865f4eaad80dce550f1d86e5d52
Sachin Sharma <sachin.kumarsharam09@gmail.com>
2023-02-06 04:07:13 +0530

    Feedback when user is typing is also implemented

--- public/chat.js
+++ public/chat.js
@@ -1,21 +1,33 @@
+// const  = require("socket.io");
+
 //Make Connection
-const scoket = io.connect("localhost:6001");
+const socket = io.connect("localhost:6001");
 
 const send = document.getElementById("send");
 const handle = document.getElementById("handle");
 const message = document.getElementById("message");
 const output = document.getElementById("output");
-
+const feedback = document.getElementById("feedback");
 //Emit Events
 send.addEventListener("click", () => {
-    scoket.emit("chat", {
+    socket.emit("chat", {
         message: message.value,
         handle: handle.value,
     });
 });
 
+message.addEventListener("keypress", () => {
+    socket.emit("typing", { handle: handle.value });
+});
+
 //Listen events
 
-scoket.on("chat", (data) => {
+socket.on("chat", (data) => {
+    feedback.innerHTML = "";
+    message.value = "";
     output.innerHTML += `<p><strong>${data.handle}: </strong> ${data.message}</p>`;
+});
+
+socket.on("typing", (data) => {
+    feedback.innerHTML = `<p><em>${data.handle} is typing </em></p>`;
 });
